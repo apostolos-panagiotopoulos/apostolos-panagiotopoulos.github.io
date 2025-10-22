051eb2db20e94b225db6ca9e97dce294263297c7
apostolos-panagiotopoulos <apostolos07.p@gmail.com>
2025-10-22 18:36:08 +0300

    Update index.c

--- index.c
+++ index.c
@@ -1,6 +1,6 @@
 # Check out solution URL
 $ cat solution.txt
-YourUsername.github.io
+apostolos-panagiotopoulos.github.io
 # Ensure the URL exists
 $ curl --output /dev/null --silent --head --fail YourUsername.github.io && \
 echo "URL exists" || echo "URL does not exist"
